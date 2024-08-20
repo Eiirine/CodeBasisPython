40d5ab01d5918b0f71435743a8f5192e4505cb8a
Irina Bessonova <glastalinka@gmail.com>
2024-08-20 11:40:56 +0300

    String
    Решение задач раздела Кодировка

---
+++ String.py
@@ -0,0 +1,13 @@
+#Напишите программу, которая выведет на экран: "Khal Drogo's favorite word is "athjahakar""
+print ('"Khal Drogo\'s favorite word is \"athjahakar\""')
+
+#Напишите программу, которая выводит на экран:
+#- Did Joffrey agree?
+#- He did. He also said "I love using \n".
+print('- Did Joffrey agree?\n- He did. He also said \"I love using \\n\".')
+
+#Выведите на экран Winter came for the House of Frey. используя конкатенацию слов.
+print('Winter' + ' came for the House of Frey.')
+
+#Используя пример выше и таблицу, выведите на экран (каждый на своей собственной строке) ~, ^ и %.
+print(chr(126)); print(chr(94)); print(chr(37));
